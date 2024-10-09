abdf57183f4e8b5f1493f4eb3a58cc214ea06a6d
kleidemos24 <h4wk27@gmail.com>
2024-10-09 18:25:03 -0500

    Added the cases for problem 4. Seems a bit funky so maybe there is something wrong with the algorithm implementation.

--- GD_CG.py
+++ GD_CG.py
@@ -52,22 +52,47 @@ class Solver:
                 x0 = x1
         return x0
 
+def build_A(n):
+    A = np.zeros(shape=(n,n))
+    for i in range(n):
+        for j in range(n):
+            A[i, j] = 1 / (1 + i+ j)
+    return A
+
+def build_b(A):
+    n = len(A)
+    b = np.zeros(n)
+    for i in range(n):
+        b[i] = 1/3 * np.sum(A[i, :])
+    return b
+
 if __name__ == '__main__':
-    A = np.array([[10, 1, 2, 3, 4],
-                  [1, 9, -1, 2, -3],
-                  [2, -1, 7, 3, -5],
-                  [3, 2, 3, 12, -1],
-                  [4, -3, -5, -1, 15]])
-    b = np.array([12, -27, 14, -17, 12])
-    x0 = np.array([0, 0, 0, 0, 0])
-    max_iter, rtol = 100, 1e-6
-    Sol = Solver(x0, A, b, max_iter, rtol, False)
-    x_sol = Sol.Gradient_Descent()
-    print(f"The solution using Gradient Descent is: {x_sol}\n")
-    x_sol = Sol.Conjugate_Gradient()
-    print(f"The solution using Conjugate Gradient is: {x_sol}")     
-    Sol = JGS.Solver(A, b, max_iter, rtol, False)
-    x_sol = Sol.Jacobi()
-    print(f"The solution using Jacobi Method is: {x_sol}")
-    x_sol = Sol.Gauss_Seidel()
-    print(f"The solution using Gauss Seidel is: {x_sol}")
+    N = [16, 32]
+    for n in N:
+        A = build_A(16)
+        b = build_b(A)
+        x0 = np.zeros(16)
+        Sol = Solver(x0, A, b, 1000, 1e-6, False)
+        x_sol = Sol.Gradient_Descent()
+        print(f"For {n}, the solution using Gradient Descent is: {x_sol}\n")
+        x_sol = Sol.Conjugate_Gradient()
+        print(f"For {n}, the solution using Conjugate Gradient is: {x_sol}\n")   
+
+    # A = np.array([[10, 1, 2, 3, 4],
+    #               [1, 9, -1, 2, -3],
+    #               [2, -1, 7, 3, -5],
+    #               [3, 2, 3, 12, -1],
+    #               [4, -3, -5, -1, 15]])
+    # b = np.array([12, -27, 14, -17, 12])
+    # x0 = np.array([0, 0, 0, 0, 0])
+    # max_iter, rtol = 100, 1e-6
+    # Sol = Solver(x0, A, b, max_iter, rtol, False)
+    # x_sol = Sol.Gradient_Descent()
+    # print(f"The solution using Gradient Descent is: {x_sol}\n")
+    # x_sol = Sol.Conjugate_Gradient()
+    # print(f"The solution using Conjugate Gradient is: {x_sol}")     
+    # Sol = JGS.Solver(A, b, max_iter, rtol, False)
+    # x_sol = Sol.Jacobi()
+    # print(f"The solution using Jacobi Method is: {x_sol}")
+    # x_sol = Sol.Gauss_Seidel()
+    # print(f"The solution using Gauss Seidel is: {x_sol}")

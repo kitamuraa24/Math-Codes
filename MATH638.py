2c649bca35f1c0be865bcfc8951ce9b640769962
kitamuraa24 <kitamuraa@tamu.edu>
2024-10-15 15:22:21 -0500

    Added to the NT scheme as well as included this in the HW file. Need to properly treat BC instead of just copying.

--- MATH638.py
+++ MATH638.py
@@ -1,5 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import Riemann_Solver as RS
+
+plt.rcParams["font.size"] = "14"
+plt.rcParams["figure.figsize"] = [8,6]
+plt.rcParams["figure.dpi"] = '300'
 
 def linear_u0(N):
     uL = -1 * np.ones(int(N/2))
@@ -23,6 +28,7 @@ def buckleyLeverett_u0(N):
 
 if __name__=="__main__":
     N = 200
+    h = 4/200
     x = np.linspace(-1.99, 1.99, N)
     CFL = 0.3
     t_end = [0.5, 2, 20]
@@ -30,3 +36,25 @@ if __name__=="__main__":
     xb = [-2, 2]
     #u0 = burgers_u0(N, x)
     #u0 = buckleyLeverett_u0(N)
+    flux_types = {'Linear': linear_u0(N), 
+                  'Burgers': burgers_u0(N, x),
+                'Buckley_Leverett': buckleyLeverett_u0(N)}
+    for flux in flux_types:
+        try:
+            u0 = flux_types[flux]
+        except:
+            "Flux type not supported"
+        params = [u0, t_end[0], CFL, h, xb, flux]
+        Solver = RS.Riemann_Solver(params, verbose=True)
+        u1 = Solver.LxF()
+        u2 = Solver.NT()
+        plt.figure(1)
+        plt.title(f"{flux}")
+        plt.grid(True)
+        plt.ylabel("u")
+        plt.xlabel("x")
+        plt.plot(x, u1, label = 'LxF')
+        plt.plot(x, u2, label='NT')
+        plt.legend()
+        plt.show()
+        
--- Riemann_Solver.py
+++ Riemann_Solver.py
@@ -13,19 +13,24 @@ class Riemann_Solver:
         self.flux_types = {"Linear": self.Linear,
                            "Burgers": self.Burgers,
                            "Buckley_Leverett": self.Buckley_Leverett}
+        
 
-    def Linear(u):
+    def Linear(self, u):
         return u
     
-    def Burgers(u):
+    def Burgers(self, u):
         return u**2/2
     
-    def Buckley_Leverett(u):
+    def Buckley_Leverett(self, u):
         return u**2/(u**2 + (1-u)**2)
     
-    def minmod(a, b):
-        if a > 0 and b > 0:
-            return a
+    def minmod(self, a, b):
+        if a >= 0 and b >= 0:
+            return min(a, b)
+        elif a <= 0 and b <= 0:
+            return max(a, b)
+        elif a*b < 0:
+            return 0
 
     def LxF(self):
         u_old = self.u0
@@ -39,7 +44,10 @@ class Riemann_Solver:
             u_max = np.max(self.u0)
             dt = self.CFL * self.h / u_max
             if t + dt > self.t_end:
-                dt = self.t_end - t
+                if k % 2 != 0:
+                    dt = self.t_end - t
+                else:
+                    dt = 0
             t+=dt
             if k % 2 == 0:
                 for i in range(len(self.u0)-1):
@@ -56,4 +64,91 @@ class Riemann_Solver:
             if self.verbose == True:
                 print(f"Time Iter No: {k} | dt: {dt:.3f} | time: {t:.3f}")
         return u_old
-            
+    
+    def NT(self):
+        #TO DO: Fix the boundary conditions: can't just copy 
+        u_old = self.u0
+        u_new = np.copy(u_old)
+        t, k = 0, 0
+        h = self.h
+        try:
+            f = self.flux_types[self.flux]
+        except:
+            "Flux not supported!"
+        while t < self.t_end:
+            u_max = np.max(self.u0)
+            dt = self.CFL * self.h / u_max
+            if t + dt > self.t_end:
+                if k % 2 != 0:
+                    dt = self.t_end - t
+                else: dt = 0
+            t+=dt
+            if k % 2 == 0:
+                for i in range(len(self.u0)-1):
+                    if i == 0:
+                        du_ip1 = u_old[i+1] - u_old[i]
+                        du_i = u_old[i] - u_old[-1]
+                        du_ip2 = u_old[i+2] - u_old[i+1]
+                        df_ip1 = f(u_old[i+1]) - f(u_old[i]) 
+                        df_i = f(u_old[i]) - f(u_old[-1])
+                        df_ip2 = f(u_old[i+2])- f(u_old[i+1])
+                    elif i == len(self.u0)-2:
+                        du_ip1 = u_old[i+1] - u_old[i]
+                        du_i = u_old[i] - u_old[i-1]
+                        du_ip2 = u_old[0] - u_old[i+1]
+                        df_ip1 = f(u_old[i+1]) - f(u_old[i])
+                        df_i = f(u_old[i]) - f(u_old[i-1])
+                        df_ip2 = f(u_old[0])- f(u_old[i+1])
+                    else:
+                        du_ip1 = u_old[i+1] - u_old[i]
+                        du_i = u_old[i] - u_old[i-1]
+                        du_ip2 = u_old[i+2] - u_old[i+1]
+                        df_ip1 = f(u_old[i+1]) - f(u_old[i])
+                        df_i = f(u_old[i]) - f(u_old[i-1])
+                        df_ip2 = f(u_old[i+2]) - f(u_old[i+1])
+                    upi = self.minmod(du_ip1, du_i)
+                    up_ip1 = self.minmod(du_ip2, du_ip1)
+                    fp_i = self.minmod(df_ip1, df_i)
+                    fp_ip1 = self.minmod(df_ip2, df_ip1)
+                    u_i_half = u_old[i] - 0.5 * dt/h * fp_i
+                    u_ip1_half = u_old[i+1] -0.5 * dt/h *fp_ip1
+                    u_new[i] = 0.5 * (u_old[i] + u_old[i+1]) + 0.125 * (upi - up_ip1) \
+                                - dt/h *(f(u_ip1_half) - f(u_i_half))
+                u_new[-1] = u_new[0]
+            else:
+                for i in range(len(self.u0), 1):
+                    if i == len(self.u0) -1:
+                        du_ip1 = u_old[0] - u_old[i]
+                        du_i = u_old[i] - u_old[i-1]
+                        du_ip2 = u_old[1] - u_old[0]
+                        df_ip1 = f(u_old[0]) - f(u_old[i])
+                        df_i = f(u_old[i]) - f(u_old[i-1])
+                        df_ip2 = f(u_old[1])- f(u_old[0])
+                    elif i == len(self.u0) - 2:
+                        du_ip1 = u_old[-1] - u_old[i]
+                        du_i = u_old[i] - u_old[i-1]
+                        du_ip2 = u_old[0] - u_old[-1]
+                        df_ip1 = f(u_old[-1]) - f(u_old[i])
+                        df_i = f(u_old[i]) - f(u_old[i-1])
+                        df_ip2 = f(u_old[0])- f(u_old[-1])
+                    else:
+                        du_ip1 = u_old[i+1] - u_old[i]
+                        du_i = u_old[i] - u_old[i-1]
+                        du_ip2 = u_old[i+2] - u_old[i+1]
+                        df_ip1 = f(u_old[i+1]) - f(u_old[i])
+                        df_i = f(u_old[i]) - f(u_old[i-1])
+                        df_ip2 = f(u_old[i+2]) - f(u_old[i+1])
+                    upi = self.minmod(du_ip1, du_i)
+                    up_ip1 = self.minmod(du_ip2, du_ip1)
+                    fp_i = self.minmod(df_ip1, df_i)
+                    fp_ip1 = self.minmod(df_ip2, df_ip1)
+                    u_i_half = u_old[i] - 0.5 * dt/h * fp_i
+                    u_ip1_half = u_old[i+1] -0.5 * dt/h *fp_ip1
+                    u_new[i] = 0.5 * (u_old[i] + u_old[i+1]) + 0.125 * (upi - up_ip1) \
+                                - dt/h *(f(u_ip1_half) - f(u_i_half))
+                u_new[0] = u_new[-1]
+            k+=1
+            u_old = np.copy(u_new)
+            if self.verbose == True:
+                print(f"Time Iter No: {k} | dt: {dt:.3f} | time: {t:.3f}")
+        return u_old

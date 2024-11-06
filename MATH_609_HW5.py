3235d0e863198862f03bf1545ea66922da1ed938
kitamuraa24 <kitamuraa@tamu.edu>
2024-11-06 08:32:29 -0600

    ALmost done with hw5

--- MATH_609_HW5.py
+++ MATH_609_HW5.py
@@ -37,7 +37,7 @@ def build_A3(a_i, b, q_i, n):
         A[i, i+1] = q_i
     return A
     
-def calc_b_i3(x, n, h, problem):
+def calc_b_i3(x, n, h):
     b = np.zeros(n)
     for i in range(n):
         b[i] = 2 + h**2*(4*x[i+ 1]**2 + 2)
@@ -76,10 +76,13 @@ def set_f (n, g, h):
 
 if __name__ == '__main__':
     u_left, u_right = 1, np.exp(1)
-    problem = 4
+    problem = 3
     if problem == 3:
         p_list = np.arange(1, 15, 1)
-        max_iter, rtol = 1000000000000000, 1e-4
+        max_iter, rtol = 50000, 1e-4
+        methods = ["Thomas-Algorithm", "Gradient-Descent", "Conjugate-Gradient"]
+        h_list = []
+        L_inf_cg_list, L_inf_ta_list, L_inf_gd_list = [], [], []
         for p in p_list:
             h = 0.5**(p)
             x = np.arange(0, 1 + h, h)
@@ -87,7 +90,7 @@ if __name__ == '__main__':
             b = calc_b_i3(x, n, h)
             a_i, q_i = -1, -1
             a, q = pre_TA3(a_i, q_i, n)
-            f = build_f3(u_left, u_right, n, h)
+            f = build_f3(u_left, u_right, n)
             if p == 1:
                 # Single forward solve
                 u_f = (np.exp(1) + 1) / (h**2*(4*x[1]**2 + 2) + 2)
@@ -95,14 +98,13 @@ if __name__ == '__main__':
                 u_f = np.insert(u_f, n, u_right)
                 phi = analytical_soln(x)
                 L_inf_f = calc_Linf_norm(u_f, phi)
-                print(f"p = {p}")
-                print(f"L_inf TA: {L_inf_f:.4e}")
+                h_list.append(h)
             else:
                 A = build_A3(a_i, b, q_i, n)
                 # Solve with LU, GD, and CG
                 u_ta = TA.thomas_alg(a, b, q, f)
-                u0 = np.zeros(n)
-                Sol = GDCG.Solver(u0, A, f, max_iter, rtol)
+                u0 = np.copy(u_ta) + 1e-6
+                Sol = GDCG.Solver(u0, A, f, max_iter, rtol, False)
                 u_gd = Sol.Gradient_Descent()
                 u_cg = Sol.Conjugate_Gradient()
                 u_ta = np.insert(u_ta, 0, u_left)
@@ -118,10 +120,26 @@ if __name__ == '__main__':
                 L_inf_ta = calc_Linf_norm(u_ta, phi)
                 L_inf_gd = calc_Linf_norm(u_gd, phi)
                 L_inf_cg = calc_Linf_norm(u_cg, phi)
-                print(f"h = {h}")
-                print(f"L_inf TA: {L_inf_ta:.4e}")
-                print(f"L_inf GD: {L_inf_gd:.4e}")
-                print(f"L_inf CG: {L_inf_cg:.4e}")
+                h_list.append(h)
+                L_inf_ta_list.append(L_inf_ta)
+                L_inf_cg_list.append(L_inf_cg)
+                L_inf_gd_list.append(L_inf_gd)
+        # Tables
+        for m in methods:
+            i = 0
+            if m == "Thomas-Algorithm":
+                L_inf = L_inf_ta_list
+            elif m == "Gradient-Descent":
+                L_inf = L_inf_gd_list
+            else:
+                L_inf = L_inf_cg_list
+            print(m)
+            for h in h_list:
+                if h == 1:
+                    print(f"h: {h} | L_inf: {L_inf_f:.4e} | L_inf/h: {L_inf_f/h:.4e} | L_inf/h^2: {L_inf_f/h**2:.4e} | L_inf/h^3: {L_inf_f/h**3:.4e}")
+                else:
+                    print(f"h: {h} | L_inf: {L_inf[i]:.4e} | L_inf/h: {L_inf[i]/h:.4e} | L_inf/h^2: {L_inf[i]/h**2:.4e} | L_inf/h^3: {L_inf[i]/h**3:.4e}")
+                    i+=1
         plt.plot(x, phi, label="Analytical solution")
         plt.legend()
         plt.grid()
@@ -157,6 +175,7 @@ if __name__ == '__main__':
         plt.legend()
         plt.show()
         plt.loglog(h_list, L_inf_list)
+        plt.grid(True)
         plt.title("Log-log plot of the error norm.")
         plt.show()
 
